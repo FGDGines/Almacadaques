18c944f61121e627c06eee66806212bcd9da440d
Deivis <deivis@debian.debian>
2023-09-21 17:08:37 -0400

    chore(DTM/FRONT): Sliding Window

--- front/src/components/ComoYDonde/ComoYDonde.tsx
+++ front/src/components/ComoYDonde/ComoYDonde.tsx
@@ -1,5 +1,6 @@
-import BtNice from "../BtNice/BtNice"
 import './ComoYDonde.css'
+import AlternateTimeline from '../TimeLine/TimeLine';
+import { tlComoYDode } from '../../data/tlComoDonde';
 
 const ComoYDonde = () => {
     return <div className="ComoYDonde">
@@ -12,14 +13,15 @@ const ComoYDonde = () => {
         <div className="ct2" style={{ color: "#75151E" }}  >
             Como y Donde ?
         </div>
-        <div  style={{ color: "#75151E" }} > 
+
+        <AlternateTimeline data={tlComoYDode} />
+
+        {/* <div  style={{ color: "#75151E" }} > 
             1. Sesiones individuales de coaching (Cadaqués/ Barcelona/ On line)
         </div>
-        <BtNice text="Saber Más" handler={()=>alert("0")} tp="white" />
         <div style={{ color: "#75151E" }}  >
             2. Experiencias de bienestar (lugares con encanto).
-        </div>
-        <BtNice text="Saber Más" handler={()=>alert("0")} tp="white" />
+        </div> */}
     </div>
 }
 
