90fc14bca30498455380303fe5037d9140ff9b98
Deivis <deivis@debian.debian>
2023-09-29 07:55:07 -0400

    fix(DTM/FRONT): Quitando letras del Home

--- front/src/components/Welcome/Welcome.tsx
+++ front/src/components/Welcome/Welcome.tsx
@@ -2,11 +2,7 @@ import './Welcome.css'
 const Welcome = ()=>{
     return <div className='Welcome'>
         <div className='ctConent'>
-            <div className='title'>
-                Almacadaques es un espacio de bienestar,<br/>
-                dirigido a mujeres que quieren<br/>
-                vivir una vida llena y con sentido.
-            </div>
+                
         </div>
     </div>
 }
