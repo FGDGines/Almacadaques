49f5e20c83515ae75c637aeb78f49a3377e3b5e9
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2023-10-10 14:56:08 -0500

    chore(JJS/FRONT):  correcion de fuentes y sesion de (Sobre mi), Agregue el resto de informacion.

--- front/src/components/Footer/Footer.tsx
+++ front/src/components/Footer/Footer.tsx
@@ -1,80 +1,58 @@
 import './Footer.css'
-import NewSletter from '../NewSletter/NewSletter';
-import NavButton from "../NavButton/NavButton";
-import { FaRegComment } from "react-icons/fa";
+
+
 const Footer = () => {
     return (
         <div className="Footer">
-            <div className="ct1">
-                <div className="ct_inf1_in2">
+            <div className="container1">
+                <div className="containerRight">
+                    <h3 className='titleFooter'>AVISO LEGAL</h3>
+                    <div className="span1"></div>
 
-                    <div className='col1'>
-
-                        <div className="inf1">
-                            <span> Avenida borbon 215 </span>
-                            <span>Barcelona, 08031</span>
-                        </div>
-                        <div className="inf1">
-                            <span className='tel'> Telf: +34 660 30 54 21</span>
-                            <span>elisabetcollvinent@gmail.com</span>
-                        </div>
-
-
-                        <span className='socialRed' >
-                           
-                            <a href="https://www.instagram.com/almacadaques/" target="_blank"> <img src="../../../src/assets/images/instagram.png" alt="Instagram" /> </a>
-                            <a href="https://www.linkedin.com/in/elisabet-coll-vinent-b9765530" target="_blank"> <img src="../../../src/assets/images/linkedin.png" alt="Linkedin" /> </a>
-                        </span>
-
-
-
-                    </div>
-                    <div className='col2'>
-
-                        <div className="inf1">
-                            <span>Sobre mi</span>
-                            <span>Coaching</span>
-                            <span>Agenda</span>
-                            <span>Colaborad@rs</span>
-                            <span>Blog</span>
-                        </div>
-
-                        <div className="btCont">
-                            <NavButton
-                                className="rounded hoverable-primary contact"
-                                target={3}
-                                text={
-                                    <>
-                                        <p>Contactar</p> <FaRegComment className="comment-icon" />
-                                    </>
-                                }
-                            />
-
-                        </div>
-
-
-
-                    </div>
-
+                    <p className='subtitlefooter'><a href='#' className='enlaceFooter'> Politica de Privacidad</a> </p>
+                    <div className="span2"></div>
+                    <p className='subtitlefooter'><a href='#' className='enlaceFooter'> Politica de coockies</a>  </p>
+                    <div className="span3"></div>
+                    <p className='subtitlefooter'><a href='#' className='enlaceFooter'> Aviso legal </a> </p>
                 </div>
-
-                <div className='col3' >
-                    <span className='span1'>¡Mantente en contacto! Obtenga las últimas actualizaciones y ofertas.</span>
-                    <NewSletter />
-                    <span className='span2'>By clicking the button you agree to the Privacy Policy and Terms and Conditions</span>
+                <div className="containerCenter">
+                    <h3 className='titleFooter'>Info Contacto </h3>
+                    <div className="span4"></div>
+                    <p className='subtitlefooter'><img src="../../../src/assets/images/whatsapp.png" alt="Telefono" className='whatsapp '/><a href='https://wa.me/+34660305421?text=Hola Almacadaques' target='_blank' className='enlaceFooter'> +34 660 30 54 21</a> </p>
+                    <div className="span5"></div>
+                    <p className='subtitlefooter' ><a href='#' target='_blank' className='enlaceFooter'> hola@almacadaques</a> </p>
+                    <div className="span6"></div>
+                    <p className='subtitlefooter'><a href='mailto:hola@almacadaques.com' target='_blank' className='enlaceFooter'> Almacadaques.com </a> </p>
+                </div>
+                <div className="containerLeft">
+                    <h3 className='titleFooter'>Newlester </h3>
+                    <div className="span7"></div>
+                    <form action="#">
+                        <p className='correo'>Direccion de correo electrónico</p>
+                        <div className="input-group">
+                            <input required type="email" name="email" autoComplete="on" className="input" />
+                            <label className="user-label">Tu direccion de correo aqui</label>
+                        </div>
+                    </form>
+                    <button className='buttonFooter' type='button'>Registro</button>
                 </div>
             </div>
-            <div className="ct2">
-                <div className="col1">
-                    <span className='ref'>Almacadaqués © 2023. Reservados todos los derechos.</span>
-                </div>
-
-                <div className="col2">
-                    <a href="#"> Terms & Conditions &nbsp;&nbsp; Privacy Policy</a>
+            <div className="container2">
+                <div className="infofooter">
+                    <div className="redes">
+                        <div className="socialRed">
+                            <a href="https://www.facebook.com/almacadaques?locale=es_LA" target='_blank'><img className="img " src="../../../src/assets/images/facebook.png" alt="Facebook" /></a>
+                            <a href="https://www.linkedin.com/in/elisabet-coll-vinent-b9765530" target='_blank'><img className="img " src="../../../src/assets/images/linkedin.png" alt="Linkedin" /></a>
+                            <a href="mailto:hola@almacadaques.com" target='_blank'><img className="img email" src="../../../src/assets/images/email.png" alt="Email" /></a>
+                            <a href="https://www.instagram.com/almacadaques" target='_blank'><img className="img " src="../../../src/assets/images/instagram.png" alt="Instagram" /></a>
+                        </div>
+                        <div className="whats">
+                            <a href="https://wa.me/+34660305421?text=Hola Almacadaques" target='_blank'><img className="img1 " src="../../../src/assets/images/whatsapp.png" alt="whatsapp"  /></a>
+                        </div>
+                    </div>
                 </div>
 
             </div>
-
         </div>
 
     );
