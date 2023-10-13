aa3953832d919362402a79cca5d02d2cd7bdbfe1
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2023-10-13 11:19:34 -0500

    Chore(JJS/FRONT): sesiones de Coaching, sin responsive.

--- front/src/components/CoachingBienestar/CoachingBienestar.tsx
+++ front/src/components/CoachingBienestar/CoachingBienestar.tsx
@@ -8,20 +8,39 @@ const CoachingBienestar = () => {
         <Navbar />
         <Franja text="Coaching para el Bienestar " />
         <div className="containerBienestar">
-            <span style={{ color: "#75151E" }}> ❝ </span>
-            ¿Sientes que existe una brecha entre la vida que llevas y la que quieres llevar? <br /> ¿Quieres convertirte en tu mejor versión?
-            <span style={{ color: "#75151E" }}> ❞ </span>
-            <p>Un proceso de coaching, respetuoso, amable y profesional, te ayudará a alcanzar tu objetivo. ¿Te acompaño?</p>
-            <h3>¿Como te puedo ayudar?</h3>
-            <p>Definiremos un objetivo claro de cambio y con las sesiones personalizadas darás un empujón y romperás los bloqueos actuales que no te permiten avanzar.
-                Te escucharé sin juzgar, te haré preguntas y te daré feedback, y con ello aclararás y ordenarás tus ideas e incrementarás tu autoconocimiento y liderazgo para descubrir nuevos enfoques y lograr aquello que quieres conseguir.
-                Como coach acompaño a la persona a través de un viaje de exploración interior. Empezamos desde donde empieza su conflicto o malestar hasta la resolución del mismo logrando así tu bienestar.</p>
 
-            
+            <div className="containerInnformacion1">
+                <div className="containerimg1">
+                    <img src="../../../src/assets/background/unnamed (2).jpg" alt="Elisabet" className='imgBienertar'/>
+                </div>
+                <div className="contseccionn1">
+                    <span style={{ color: "#75151E" }}> ❝ </span>
+                    ¿Sientes que existe una brecha entre la vida que llevas y la que quieres llevar? ¿Quieres convertirte en tu mejor versión?
+                    
+                    <span style={{ color: "#75151E" }}> ❞ </span>
+                    <p>Un proceso de coaching, respetuoso, amable y profesional, te ayudará a alcanzar tu objetivo. ¿Te acompaño?</p>
+                    <h3>¿Como te puedo ayudar?</h3>
+                    <p>Definiremos un objetivo claro de cambio y con las sesiones personalizadas darás un empujón y romperás los bloqueos actuales que no te permiten avanzar.
+                        Te escucharé sin juzgar, te haré preguntas y te daré feedback, y con ello aclararás y ordenarás tus ideas e incrementarás tu autoconocimiento y liderazgo para descubrir nuevos enfoques y lograr aquello que quieres conseguir.
+                        Como coach acompaño a la persona a través de un viaje de exploración interior. Empezamos desde donde empieza su conflicto o malestar hasta la resolución del mismo logrando así tu bienestar.</p>
+                    <p>
+                        Te escucharé sin juzgar, te haré preguntas y te daré feedback, y con ello aclararás y ordenarás tus ideas e incrementarás tu autoconocimiento y liderazgo para descubrir nuevos enfoques y lograr aquello que quieres conseguir.
+                    </p>
+                    <p>
+                        Como coach acompaño a la persona a través de un viaje de exploración interior. Empezamos desde donde empieza su conflicto o malestar hasta la resolución del mismo logrando así tu bienestar.
+                    </p>
+                    <p> <a href="">Blog de Testimonio</a> </p>
+                </div>
 
-            <span style={{ color: "#75151E" }}> ❝ </span>
-            Cuando uno toma la determinación de liderar su vida, posiblemente después de alguna crisis o desajuste, el acompañamiento de un coach lo puede ayudar a acelerar y lograr mejores resultados
-            <span style={{ color: "#75151E" }}> ❞ </span>
+            </div>
+
+            <div className="containerInnformacion2">
+                <span style={{ color: "#75151E" }}> ❝ </span>
+                Cuando uno toma la determinación de liderar su vida, posiblemente después de alguna crisis o desajuste, el acompañamiento de un coach lo puede ayudar a acelerar y lograr mejores resultados
+                <span style={{ color: "#75151E" }}> ❞ </span>
+                <p className='textFormulario'>Pide más información sin compromiso</p>
+            </div>
+
 
         </div>
         <Footer />
--- front/src/components/CoachingInternacional/CoachingInternacional.tsx
+++ front/src/components/CoachingInternacional/CoachingInternacional.tsx
@@ -8,13 +8,28 @@ const CoachingInternacional = () => {
         <Navbar />
         <Franja text="Coaching & mentoring internacional" />
         <div className="containerInternacional">
-            <h3>PREGUNTAS DESTACADAS:</h3>
-            <p>¿quieres incrementar tu facturación de ventas en internacional?</p>
-            <p>
-¿quieres mejorar el rendimiento de tu equipo internacional?</p>
+        <div className="containerInnformacion1">
+                <div className="containerimg2">
+                    <img src="../../../src/assets/background/unnamed (2).jpg" alt="Elisabet" className='imgBienertar'/>
+                </div>
+                <div className="contseccionn1">
+                    <h3>PREGUNTAS DESTACADAS:</h3>
+                    <p>¿quieres incrementar tu facturación de ventas en internacional?</p>
+                    <p>¿quieres mejorar el rendimiento de tu equipo internacional? </p>
+                    <p>¿eres un export manager y te planteas un cambio profesional?</p>
+                    <p>¿eres un expat y estás experimentando un vaivén en tu adaptación?</p>                    
+                    <p>Ya seas export manager o CEO de la empresa el proceso de crecimiento internacional puede ser más que retador y conllevar algún desajuste. <br /> Un proceso de coaching y mentoring te ayudará a gestionar los cambios de manera amable para llegar a los objetivos que te plantees. <b>¿Te acompaño?</b> </p>
+                </div>
 
-<p>¿eres un export manager y te planteas un cambio profesional?</p>
-<p>¿eres un expat y estás experimentando un vaivén en tu adaptación?</p>
+            </div>
+
+            <div className="containerInnformacion2">
+                <h3>¿Como te puedo ayudar?</h3>
+                <p>Como coach te escucho sin juzgar, te hago preguntas y te doy feedback, y con ello aclararás y ordenarás tus ideas e incrementarás tu autoconocimiento y liderazgo para descubrir nuevos enfoques y lograr aquello que quieres conseguir.</p>
+                <p>Como mentora internacional te comparto mis propias experiencias y con mucha empatía y sensibilidad te acompaño a alcanzar los objetivos que quieres lograr, logrando así mayor bienestar.</p>
+                <p>Contácta sin compromiso para definir juntos como te puedo ayudar a conseguir los resultados que buscas.</p>
+                <p className='textFormulario'>Pide más información sin compromiso</p>
+            </div>
         </div>
         <Footer />
     </div>
