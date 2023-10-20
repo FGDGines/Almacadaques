d8f09f6a3f29242126afecb70a3a74b2936342b6
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2023-10-20 11:50:28 -0500

    Chore(JJS/FRONT):informacion de colaboradores agregada.

--- front/src/data/colaboradores.ts
+++ front/src/data/colaboradores.ts
@@ -7,11 +7,11 @@ export interface Colaborador {
 }
 export const ListaColaboradores: Colaborador[] = [
 {
-    nombre: 'Mercedes Cazas',
-    cargo: 'Instructora de Movimiento Creativo',
-    descripcion: 'Creadora de Happy Dance, una propuesta de danza lúdica creada para disfrutar y ganar consciencia física y energética',
+    nombre: 'Mercé Cazes',
+    cargo: 'Educadora de Masaje Infantil',
+    descripcion: 'Canalizadora, facilitadora y formadora de la técnica de sanación energética TASI (Terapia Avanzada de Salud Integral)',
     imagen: '../../src/assets/imagen_colaboradores/colaborador1.jpeg',
-    contacto: 'https://www.instagram.com/happy_dance_paz/',
+    contacto: 'https://mercecazes.com/',
 },
 {
     nombre: 'Paz Jiménez',
@@ -22,17 +22,17 @@ export const ListaColaboradores: Colaborador[] = [
 },
 {
     nombre: 'Monti Serra',
-    cargo: 'Instructora de Movimiento Creativo',
-    descripcion: 'Creadora de Happy Dance, una propuesta de danza lúdica creada para disfrutar y ganar consciencia física y energética',
+    cargo: 'Entrenadora emocional especialitzada en el Benestar Emocional',
+    descripcion: 'Acompaña a empresas y organizaciones para que sus profesionales y equipos mejoren, gracias a la comunicación emocional aplicada al liderazgo y al trabajo en equipo. Conferenciante y escritora',
     imagen: '../../src/assets/imagen_colaboradores/colaborador3.jpeg',
-    contacto: 'https://www.instagram.com/happy_dance_paz/',
+    contacto: 'https://www.montiserra.com/ca/',
 },
 {
     nombre: 'Maribel Salvador',
-    cargo: 'Instructora de Movimiento Creativo',
+    cargo: 'CEO maribelsalvador.com',
     descripcion: 'Creadora de Happy Dance, una propuesta de danza lúdica creada para disfrutar y ganar consciencia física y energética',
     imagen: '../../src/assets/imagen_colaboradores/colaboradores4.jpeg',
-    contacto: 'https://www.instagram.com/happy_dance_paz/',
+    contacto: 'https://www.linkedin.com/in/maribel-salvador-alcaraz-formacion/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
 }
   
 ]
