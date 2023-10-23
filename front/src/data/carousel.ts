3c418e703437de97786fad493f41560249a24857
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2023-10-22 21:54:47 -0500

    Chore(JJS/FRONT): Orden de imagenes de carrousell y agregagando un autor e imagenes mas

--- front/src/data/carousel.ts
+++ front/src/data/carousel.ts
@@ -2,37 +2,44 @@ import { tpCarouelItem } from "../types/typesComponents";
 
 export const carouselDescription: tpCarouelItem[] = [
     {
-        id: 1,
-        title: '¿Te sientes estresad@?',
-        src: './src/assets/Fotos_Home/1.jpg',
+        id: 6,
+        title: '¿Quieres ser tu mejor versión?',
+        src: '../../src/assets/carrousell/sexta.png',
         autor: '@ejemplo',
         link_autor: "https://www.google.com/search?q=fotos+de+Bienestar&oq=fotos+de+Bienestar&aqs=chrome..69i57j0i512l9.5406j0j7&sourceid=chrome&ie=UTF-8"
     },
     {
-        id: 2,
-        title: '¿Quieres un cambio de aires?',
-        src: './src/assets/Fotos_Home/2.jpg',
+        id: 5,
+        title: '¿Necesitas inspiración?',
+        src: '../../src/assets/carrousell/quinta.jpg',
+        autor: '@ejemplo',
+        link_autor: "https://www.google.com/search?q=fotos+de+Bienestar&oq=fotos+de+Bienestar&aqs=chrome..69i57j0i512l9.5406j0j7&sourceid=chrome&ie=UTF-8"
+    },
+    {
+        id: 4,
+        title: '¿Quieres hacer un cambio y no sabes por dónde empezar?',
+        src: '../../src/assets/carrousell/cuarta.jpg',
         autor: '@ejemplo',
         link_autor: "https://www.google.com/search?q=fotos+de+Bienestar&oq=fotos+de+Bienestar&aqs=chrome..69i57j0i512l9.5406j0j7&sourceid=chrome&ie=UTF-8"
     },
     {
         id: 3,
         title: '¿Sientes que estás bloquead@?',
-        src: './src/assets/Fotos_Home/3.jpg',
+        src: '../../src/assets/carrousell/tercera.jpg',
         autor: '@ejemplo',
         link_autor: "https://www.google.com/search?q=fotos+de+Bienestar&oq=fotos+de+Bienestar&aqs=chrome..69i57j0i512l9.5406j0j7&sourceid=chrome&ie=UTF-8"
     },
     {
-        id: 4,
-        title: '¿Necesitas inspiración?',
-        src: './src/assets/Fotos_Home/4.jpg',
+        id: 2,
+        title: '¿Quieres un cambio de aires?',
+        src: '../../src/assets/carrousell/segunda.jpg',
         autor: '@ejemplo',
         link_autor: "https://www.google.com/search?q=fotos+de+Bienestar&oq=fotos+de+Bienestar&aqs=chrome..69i57j0i512l9.5406j0j7&sourceid=chrome&ie=UTF-8"
     },
     {
-        id: 5,
+        id: 1,
         title: '¿Quieres ser tu mejor versión?',
-        src: './src/assets/Fotos_Home/5.jpg',
+        src: '../../src/assets/carrousell/primera.jpg',
         autor: '@ejemplo',
         link_autor: "https://www.google.com/search?q=fotos+de+Bienestar&oq=fotos+de+Bienestar&aqs=chrome..69i57j0i512l9.5406j0j7&sourceid=chrome&ie=UTF-8"
     }
