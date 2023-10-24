704bca6e5136ec59131b661582f25c58c70e871a
Deivis <deivis@dtm.debiab.com>
2023-10-24 14:39:17 -0400

    chore(DTM/BACK): api/auth/register

--- back/src/Server/api/controllers/auth/Register.ts
+++ back/src/Server/api/controllers/auth/Register.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { DataUser, User } from "../../../db/models";
+import bycrypt from 'bcrypt'
 
 
 export const Register = async (req: Request, res: Response) => {
@@ -14,6 +15,9 @@ export const Register = async (req: Request, res: Response) => {
         const id_data_user = tDataUser.id
 
         tUser = new User({ correo, password, id_data_user })
+        const salt = bycrypt.genSaltSync(10)
+        const password_hash = bycrypt.hashSync(password , salt)
+        tUser.password = password_hash
         await tUser.save()
 
         return res.status(200).json({ status: 200, msg: "Usuario registrado" })
