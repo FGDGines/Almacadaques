d4af058c129444257077aa99140b06266f046942
Deivis <deivis@dtm.debiab.com>
2023-10-27 12:42:14 -0400

    chore(DTM/BACK): api/testimonios 50%

--- back/src/Server/api/controllers/testimony/Create.ts
+++ back/src/Server/api/controllers/testimony/Create.ts
@@ -1,11 +1,19 @@
 import { Request, Response } from "express";
+import { DataTestimonio, Testimonio } from "../../../db/models";
 
 export const Create = async ( req: Request ,res: Response)=>{
     const {body} = req
+    const {witness,testimony_es,testimony_en,testimony_cat}  = body
 
     try{
-        return res.status(200).json({status: 200, msg: "Crear testimonio Online" , body})
+        const tDataTestimonio = new DataTestimonio({es:testimony_es , cat:testimony_cat, en:testimony_en})
+        await tDataTestimonio.save()
+
+        const tTestimonio = new Testimonio({witness, id_data: tDataTestimonio.id})
+        await tTestimonio.save()
+
+        return res.status(200).json({status: 200, msg: "Testimonio Creado" })
     }catch(err){
-        return res.status(200).json({status: 500, err , msg: ""})
+        return res.status(200).json({status: 500, err , msg: "No podemos crear testimonios en este momento"})
     }
 }
--- back/src/Server/api/routes/testimony.ts
+++ back/src/Server/api/routes/testimony.ts
@@ -3,13 +3,19 @@ import { validarCampos } from "../../middlewares/ValidarCampos";
 import { security_post } from "../../middlewares/Security";
 import { Create } from "../controllers/testimony/Create";
 import { check } from "express-validator";
+import { Delete } from "../controllers/testimony/Delete";
 
 const app = Router()
 
 app.post('/create' , [security_post  ,
     check('witness', 'El nombre del testigo es obligatorio').not().isEmpty(),
-    check('testimony','El testomonio es obligatorio').not().isEmpty(),
-    check('imagen','La imagen es obligatoria').not().isEmpty()
+    check('testimony_es','El testomonio en español es obligatorio').not().isEmpty(),
+    check('testimony_en','El testomonio en ingles es obigatorio').not().isEmpty(),
+    check('testimony_cat','El testomonio en catalan es obligatorio').not().isEmpty()
     , validarCampos] , Create)
+
+app.post('/delete', [security_post , 
+    check('id','El testimonio a eliminar es obligatorio').isNumeric()
+     , validarCampos] , Delete)
 
 module.exports = app
