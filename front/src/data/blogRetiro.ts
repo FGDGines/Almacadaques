33075a073b43011f2db8bfd886128dfc67cb33dd
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2023-10-27 16:08:21 -0500

    Chore(JJS/FRONT): Blog de Retiros

--- front/src/data/blogRetiro.ts
+++ front/src/data/blogRetiro.ts
@@ -3,29 +3,41 @@ const retiroInfo = [
     {
       index: 1,
       day: 26,
-      month: 7,
+      month: 8,
       year: 2023,
-      image: "./src/assets/images/blog-line-1-620x464.jpg",
+      image: [
+        "../../src/assets/carouselBlog/img1.jpg",
+        "./src/assets/images/blog-line-1-620x464.jpg",
+        "./src/assets/images/blog-line-1-620x464.jpg",
+      ],
       title: "The ultimate guide to time management",
       author: "Andres Brito",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique inventore eaque quidem blanditiis aperiam, non harum nesciunt hic est temporibus molestias aliquam. Perspiciatis velit nulla libero eveniet expedita nam corrupti, inventore odit tempore. Et, ad architecto! Error deserunt nesciunt veniam impedit repellendus soluta ut officia sint qui aut quos corrupti corporis ratione, dolores, quo id omnis. Consectetur quidem iste cupiditate quibusdam ab possimus omnis? Excepturi, quaerat corporis beatae eligendi reprehenderit aperiam sequi nemo officia nobis doloremque temporibus tempora possimus veniam quasi nostrum facilis tenetur, non, modi iste expedita inventore perferendis deserunt atque! Iure repellendus magni vitae ducimus atque porro distinctio."
     },
     {
       index: 2,
-      day: 15,
+      day: 24,
       month: 8,
       year: 2023,
-      image: "./src/assets/images/blog-line-1-620x464.jpg",
+      image: [
+        "../../src/assets/carouselBlog/img2.jpg",
+        "./src/assets/images/blog-line-1-620x464.jpg",
+        "./src/assets/images/blog-line-1-620x464.jpg",
+      ],
       title: "Effective Communication Strategies",
       author: "Ana Sánchez",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique inventore eaque quidem blanditiis aperiam, non harum nesciunt hic est temporibus molestias aliquam. Perspiciatis velit nulla libero eveniet expedita nam corrupti, inventore odit tempore. Et, ad architecto! Error deserunt nesciunt veniam impedit repellendus soluta ut officia sint qui aut quos corrupti corporis ratione, dolores, quo id omnis. Consectetur quidem iste cupiditate quibusdam ab possimus omnis? Excepturi, quaerat corporis beatae eligendi reprehenderit aperiam sequi nemo officia nobis doloremque temporibus tempora possimus veniam quasi nostrum facilis tenetur, non, modi iste expedita inventore perferendis deserunt atque! Iure repellendus magni vitae ducimus atque porro distinctio."
     },
     {
       index: 3,
-      day: 21,
+      day: 29,
       month: 9,
       year: 2023,
-      image: "./src/assets/images/blog-line-1-620x464.jpg",
+      image: [
+        "../../src/assets/carouselBlog/img3.jpg",
+        "./src/assets/images/blog-line-1-620x464.jpg",
+        "./src/assets/images/blog-line-1-620x464.jpg",
+      ],
       title: "Achieving Work-Life Balance",
       author: "Luis Ramirez",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique inventore eaque quidem blanditiis aperiam, non harum nesciunt hic est temporibus molestias aliquam. Perspiciatis velit nulla libero eveniet expedita nam corrupti, inventore odit tempore. Et, ad architecto! Error deserunt nesciunt veniam impedit repellendus soluta ut officia sint qui aut quos corrupti corporis ratione, dolores, quo id omnis. Consectetur quidem iste cupiditate quibusdam ab possimus omnis? Excepturi, quaerat corporis beatae eligendi reprehenderit aperiam sequi nemo officia nobis doloremque temporibus tempora possimus veniam quasi nostrum facilis tenetur, non, modi iste expedita inventore perferendis deserunt atque! Iure repellendus magni vitae ducimus atque porro distinctio."
