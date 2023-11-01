1b60a93685eda75840a87b8713900bea721150d6
Daniel Balasnoa <65874667+Daniel20020710@users.noreply.github.com>
2023-11-01 04:59:44 -0500

    Chore:(DBP/Front): Blog Podcast

--- front/src/data/listPodcast.ts
+++ front/src/data/listPodcast.ts
@@ -3,28 +3,41 @@ import { AudioPlayerProps } from "../types/typesComponents";
 export const podcastData:AudioPlayerProps[] =  [
       {
         url: "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3",
-        title: "Música Relajante para el Alma",
-        author: "Daniel Suarez",
-        thumbnail: "https://cdn.wallpapersafari.com/2/29/fdr5Z7.jpg",
-        reseña: "Sumérgete en la tranquilidad con esta música relajante. Ideal para la meditación y el bienestar personal.",
+        titulo: "Música Relajante para el Alma",
+        autor: "Daniel Suarez",
+        imagen: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiwcvQsM8-g8UhqIkYhqovspXo29w2py-MZLaIBlHXL9w6X15KhrwmQrEplNiMeVNnrkZsYznXjYFlLXwJ0a-cEI5OQv7_MUv1MF5xc434aM88hZZ-JnU-Id8EGWHOcAXCBAkEZL3VKjCWE-BtfLP7p8UoO50SZ0rOGd2OtV1gz2hDOfxd3UP6AG3TViQ/w1600/4k-pc-wallpaper-ai-art.webp",
         fecha: 'October 17, 2023'
       },
       {
         url: "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3",
-        title: "Ciencia Ficción",
-        author: "Rodrigo Lopez",
-        thumbnail: "https://img.freepik.com/foto-gratis/pintura-nave-espacial-levantada-galaxia_123827-23492.jpg",
-        reseña: "Viaja a mundos lejanos y desconocidos con estas emocionantes historias de ciencia ficción narradas por el autor.",
+        titulo: "Ciencia Ficción",
+        autor: "Rodrigo Lopez",
+        imagen: "https://wallpapershome.com/images/pages/pic_h/24953.jpeg",
         fecha: 'October 17, 2023'
       },
       {
         url: "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3",
-        title: "Cocina Gourmet",
-        author: "Maria Guevara",
-        thumbnail: "https://img.freepik.com/foto-gratis/chef-colocando-hierba-comida-gourmet_23-2148516908.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1698278400&semt=ais",
-        reseña: "Descubre los secretos detrás de los platos más deliciosos con las recetas exclusivas de este chef de renombre.",
+        titulo: "Cocina Gourmet",
+        autor: "Maria Guevara",
+        imagen: "https://pixewall.com/wp-content/uploads/2023/04/Retrowave-Car-4k-Wallpapers.jpg",
         fecha: 'October 17, 2023'
       }
+      ,
+      {
+        url: "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3",
+        titulo: "Ciencia Ficción",
+        autor: "Rodrigo Lopez",
+        imagen: "https://wallpapershome.com/images/pages/pic_h/24953.jpeg",
+        fecha: 'October 17, 2023'
+      },
+      {
+        url: "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3",
+        titulo: "Música Relajante para el Alma",
+        autor: "Daniel Suarez",
+        imagen: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiwcvQsM8-g8UhqIkYhqovspXo29w2py-MZLaIBlHXL9w6X15KhrwmQrEplNiMeVNnrkZsYznXjYFlLXwJ0a-cEI5OQv7_MUv1MF5xc434aM88hZZ-JnU-Id8EGWHOcAXCBAkEZL3VKjCWE-BtfLP7p8UoO50SZ0rOGd2OtV1gz2hDOfxd3UP6AG3TViQ/w1600/4k-pc-wallpaper-ai-art.webp",
+        fecha: 'October 17, 2023'
+      }
+      
     ]
   
 
