7c70b81c44c45bdfd5d4412036f3426416625531
Daniel Balasnoa <65874667+Daniel20020710@users.noreply.github.com>
2023-11-03 21:41:16 -0500

    Chore:(DBP/Front): Ajustes Blog Podcast
    Buscador añadido y flecha de desplazamiento

--- front/src/components/Podcast/Podcast.tsx
+++ front/src/components/Podcast/Podcast.tsx
@@ -2,28 +2,96 @@ import './Podcast.css'
 import Navbar from '../Navbar/Navbar';
 import Franja from '../Franja/Franja';
 import Footer from '../Footer/Footer';
-import FormDefault from '../FormDefault/FormDefault';
-import { formDataToObject } from '../../helpers/Forms';
-import { fetchDefault } from '../../helpers/Server';
 import { podcastData } from "../../data/listPodcast";
 import { AudioP } from '../ItemPodcast/ItemPodcast'
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { AudioPlayerProps } from '../../types/typesComponents';
 
 
-
+const removeAccents = (text: string) => {
+    return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+};
 
 const Podcast = () => {
 
-    const enviarFormulario = (bag: FormData) => {
-        bag.set('fragment', 'Contactar')
-        fetchDefault('/mail/create', { body: JSON.stringify(formDataToObject(bag)) })
-    }
+
     const [currentPodcast, setCurrentPodcast] = useState<AudioPlayerProps | null>(podcastData[0] || null); // Establecer el primer podcast como inicial
 
     const handlePlayPodcast = (podcast: AudioPlayerProps) => {
         setCurrentPodcast(podcast);
     };
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState<AudioPlayerProps[]>([]);
+
+    const containerRecentRef = useRef<HTMLDivElement>(null);
+    const containerPopularRef = useRef<HTMLDivElement>(null);
+ 
+    const [reachedEnd, setReachedEnd] = useState(false);
+
+    const [containerPodWidth, setContainerPodWidth] = useState<number>(0);
+    
+    useEffect(() => {
+        const calculateContainerWidth = (containerRef: React.RefObject<HTMLDivElement>) => {
+            if (containerRef.current) {
+                const container = containerRef.current;
+                const firstElement = container.querySelector('.containerPod');
+                if (firstElement) {
+                    const elementStyles = window.getComputedStyle(firstElement);
+                    const elementWidth = firstElement.getBoundingClientRect().width;
+                    const marginRight = parseFloat(elementStyles.marginRight || '0');
+                    setContainerPodWidth(elementWidth + marginRight);
+                }
+            }
+        };
+
+        calculateContainerWidth(containerRecentRef);
+        calculateContainerWidth(containerPopularRef);
+    }, []);
+
+    const handleNextSlide = (containerRef: React.RefObject<HTMLDivElement>) => {
+        if (containerRef.current) {
+            const container = containerRef.current;
+
+            // Verifica si el scroll alcanzó el final
+            const atEnd = container.scrollLeft + container.clientWidth >= container.scrollWidth;
+            
+            if (atEnd) {
+                container.scrollTo({ left: 0, behavior: 'smooth' });
+            } else {
+                container.scrollTo({
+                    left: container.scrollLeft + containerPodWidth,
+                    behavior: 'smooth',
+                });
+            }
+        }
+    };
+
+
+
+
+
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const term = event.target.value;
+        setSearchTerm(term);
+
+        if (term.trim() === '') {
+            setSearchResults([]); // Limpiar los resultados si el término de búsqueda está vacío
+        } else {
+            const normalizedSearchTerm = removeAccents(term.toLowerCase());
+
+            const filteredPodcasts = podcastData.filter(podcast =>
+                podcast.titulo && removeAccents(podcast.titulo.toLowerCase()).includes(normalizedSearchTerm)
+            );
+            setSearchResults(filteredPodcasts);
+        }
+    };
+
+    const handleSelectPodcast = (podcast: AudioPlayerProps) => {
+        setCurrentPodcast(podcast);
+        setSearchTerm(''); // Limpiar el término de búsqueda al seleccionar un podcast
+    };
+
 
     return <div className="Podcast">
         <Navbar />
@@ -36,10 +104,34 @@ const Podcast = () => {
                     <label className="search-icon">
                         <i className="fas fa-search"></i>
                     </label>
-                    <input type="search" name="buscador" id="" placeholder='Buscar' />
+                    <input type="search" name="buscador" id="" placeholder='Buscar' value={searchTerm}
+                        onChange={handleSearch} />
                 </div>
             </div>
 
+            {searchTerm.trim() !== '' && ( // Mostrar los resultados solo si hay un término de búsqueda
+                <div className="ctResultadosBusqueda">
+                    {searchResults.length === 0 ? (
+                        <p className='mensaje'>No hay resultados para '{searchTerm}'</p>
+                    ) : (
+                        searchResults.map((podcast, index) => (
+                            <div className='containerPod' key={index} onClick={() => handleSelectPodcast(podcast)}>
+                                {podcast.titulo && ( // Verificación de existencia de 'titulo'
+                                    <AudioP
+                                        url={podcast.url}
+                                        titulo={podcast.titulo}
+                                        imagen={podcast.imagen}
+                                        mostrarDatos={true}
+                                        fecha={podcast.fecha}
+                                        autor={podcast.autor}
+                                        mostrarControles={false}
+                                    />
+                                )}
+                            </div>
+                        ))
+                    )}
+                </div>
+            )}
             <div className="ctCategorias">
 
                 <div className="ctTituloCategoria">
@@ -74,9 +166,17 @@ const Podcast = () => {
                     </h2>
                 </div>
 
-                <div className="listaPodcast">
+
+
+                <div className="arrow-left">
+                    <i className="fas fa-arrow-left"></i>
+                </div>
+
+
+                <div className="listaPodcast" ref={containerRecentRef} style={{ display: 'flex', overflowX: 'scroll' }}>
+
                     {podcastData.map((podcast, index) => (
-                        <div className='containerPod' key={index} onClick={() => handlePlayPodcast(podcast)}>
+                        <div id='#containerPod' className='containerPod' key={index} onClick={() => handlePlayPodcast(podcast)}>
                             <AudioP
                                 url={podcast.url}
                                 titulo={podcast.titulo}
@@ -88,9 +188,11 @@ const Podcast = () => {
                             />
                         </div>
                     ))}
-
                 </div>
 
+                <div className="arrow-right" onClick={() => handleNextSlide(containerRecentRef)}>
+                    <i className="fa-regular fa-chevron-right"></i>
+                </div>
 
             </div>
             <div className="ctMasvistos">
@@ -100,7 +202,13 @@ const Podcast = () => {
                     </h2>
                 </div>
 
-                <div className="listaPodcast">
+                <div className="arrow-left" >
+                    <i className="fas fa-arrow-left"></i>
+                </div>
+
+
+                <div className="listaPodcast" ref={containerPopularRef} style={{ display: 'flex', overflowX: 'scroll' }}>
+
                     {podcastData.map((podcast, index) => (
                         <div className='containerPod' key={index} onClick={() => handlePlayPodcast(podcast)}>
                             <AudioP
@@ -115,6 +223,12 @@ const Podcast = () => {
                         </div>
                     ))}
                 </div>
+
+                <div className="arrow-right" onClick={() => handleNextSlide(containerPopularRef)}>
+                    <i className="fa-regular fa-chevron-right"></i>
+                </div>
+
+
 
             </div>
 
