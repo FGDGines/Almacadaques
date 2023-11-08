b97f3f50f8079c5cf4e87dd78188269c7288cc66
Daniel Balasnoa <65874667+Daniel20020710@users.noreply.github.com>
2023-11-08 14:24:55 -0500

    Chore:(DBP/Front): añadiendo diseño a inicio

--- front/src/Dashboard/pages/Inicio/InicioAdmin.tsx
+++ front/src/Dashboard/pages/Inicio/InicioAdmin.tsx
@@ -1,14 +1,22 @@
 import './Inicio.css'
 import { NarbarAdmin } from '../../components/NarbarAdmin/NarbarAdmin'
 import { BarSession } from '../../components/barSession/barSession'
+import { ItemCarrusel } from '../../components/ItemCarrusel/ItemCarrusel'
 
 function InicioAdmin() {
   return (
     <div className="inicioAdmin">
       <NarbarAdmin />
-      <BarSession tituloVista='Inicio' segundoTitulo='Dashboard' nombre='Kristine'  img='../../../../src/assets/Dashboard-almacadaques/users/user.svg'/>
+      <div className="contenidoPrincipal">
+        <BarSession tituloVista='Inicio' segundoTitulo='Dashboard' nombre='Kristine' img='../../../../src/assets/Dashboard-almacadaques/users/user.svg' />
+        <ItemCarrusel />
+
+        <div className="contenidoBienestar">
+            
 
 
+        </div>
+      </div>
     </div>
 
 
