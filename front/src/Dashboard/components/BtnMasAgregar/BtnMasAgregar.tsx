79b255a13042ca59033ba0a36eb5714068301b6e
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2023-11-08 15:04:45 -0500

    chore(JJS/FRONT): diseño de Session de sobre mi

---
+++ front/src/Dashboard/components/BtnMasAgregar/BtnMasAgregar.tsx
@@ -0,0 +1,14 @@
+import './btnMasAgregar.css'
+
+function BtnMasAgregar() {
+  return (
+    <div className='Buton'>
+      <a href="#" className='EnlaceBton'>
+        <img src="../../../../src/assets/Dashboard-almacadaques/iconBtn/mingcute_add-line.svg" alt="" />
+        Agregar
+      </a>
+    </div>
+  )
+}
+
+export  { BtnMasAgregar }
