4b85cd7eb991f0b1ae27743e9dba16b2ec3fb20b
Daniel Balasnoa <65874667+Daniel20020710@users.noreply.github.com>
2023-11-08 17:51:11 -0500

    Chore:(DBP/Front): Ajustes dashboard Inicio

--- front/src/Dashboard/components/ItemCarrusel/ItemCarrusel.tsx
+++ front/src/Dashboard/components/ItemCarrusel/ItemCarrusel.tsx
@@ -11,45 +11,125 @@ export const ItemCarrusel = () => {
                 <div className="tituloCarrusel"><p>Item de Carrousel</p></div>
                 <div className="botonesCarrusel">
 
-                    <i className='left'><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/left.svg" alt="" /></i>
+                    <i className='left'><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/left.svg" alt="Left" /></i>
 
 
-                    <i className='right'><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/right.svg" alt="" /></i>
+                    <i className='right'><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/right.svg" alt="Right" /></i>
                 </div>
             </div>
 
             <div className="itemsCarrusel">
 
                 <div className="items">
-                    <img src="" alt="" />
-                    <div className="ctOverlay">
-                        <p>Es importante la salud Mental</p>
+                    <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg" alt="Fondo Items" />
+                    <div className="ctOver">
+
+                        <div className="iconosItems">
+                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Borrar.svg" alt="Borrar" /></i>
+                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Editar.svg" alt="Editar" /></i>
+                        </div>
+
+                        <div className="contenidoItems">
+                            <div className='ItemsDescripcion'>Es importante la salud mental</div>
+                            <div className="firma">
+                                firma
+                            </div>
+                        </div>
+
                     </div>
                 </div>
                 <div className="items">
-                    <img src="" alt="" />
-                    <div className="ctOverlay">
-                        <p>Es importante la salud Mental</p>
+                    <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg" alt="Fondo Items" />
+                    <div className="ctOver">
+
+                        <div className="iconosItems">
+                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Borrar.svg" alt="Borrar" /></i>
+                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Editar.svg" alt="Editar" /></i>
+                        </div>
+
+                        <div className="contenidoItems">
+                            <div className='ItemsDescripcion'>Es importante la salud mental</div>
+                            <div className="firma">
+                                firma
+                            </div>
+                        </div>
+
                     </div>
                 </div>
                 <div className="items">
-                    <img src="" alt="" />
-                    <div className="ctOverlay">
-                        <p>Es importante la salud Mental</p>
+                    <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg" alt="Fondo Items" />
+                    <div className="ctOver">
+
+                        <div className="iconosItems">
+                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Borrar.svg" alt="Borrar" /></i>
+                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Editar.svg" alt="Editar" /></i>
+                        </div>
+
+                        <div className="contenidoItems">
+                            <div className='ItemsDescripcion'>Es importante la salud mental</div>
+                            <div className="firma">
+                                firma
+                            </div>
+                        </div>
+
                     </div>
                 </div>
                 <div className="items">
-                    <img src="" alt="" />
-                    <div className="ctOverlay">
-                        <p>Es importante la salud Mental</p>
+                    <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg" alt="Fondo Items" />
+                    <div className="ctOver">
+
+                        <div className="iconosItems">
+                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Borrar.svg" alt="Borrar" /></i>
+                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Editar.svg" alt="Editar" /></i>
+                        </div>
+
+                        <div className="contenidoItems">
+                            <div className='ItemsDescripcion'>Es importante la salud mental</div>
+                            <div className="firma">
+                                firma
+                            </div>
+                        </div>
+
                     </div>
                 </div>
                 <div className="items">
-                    <img src="" alt="" />
-                    <div className="ctOverlay">
-                        <p>Es importante la salud Mental</p>
+                    <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg" alt="Fondo Items" />
+                    <div className="ctOver">
+
+                        <div className="iconosItems">
+                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Borrar.svg" alt="Borrar" /></i>
+                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Editar.svg" alt="Editar" /></i>
+                        </div>
+
+                        <div className="contenidoItems">
+                            <div className='ItemsDescripcion'>Es importante la salud mental</div>
+                            <div className="firma">
+                                firma
+                            </div>
+                        </div>
+
                     </div>
                 </div>
+                <div className="items">
+                    <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg" alt="Fondo Items" />
+                    <div className="ctOver">
+
+                        <div className="iconosItems">
+                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Borrar.svg" alt="Borrar" /></i>
+                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Editar.svg" alt="Editar" /></i>
+                        </div>
+
+                        <div className="contenidoItems">
+                            <div className='ItemsDescripcion'>Es importante la salud mental</div>
+                            <div className="firma">
+                                firma
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
 
             </div>
 
