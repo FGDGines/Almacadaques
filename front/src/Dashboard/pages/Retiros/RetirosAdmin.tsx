78e81dca6bc8658ed72bd9f7d1a9ee741491c6fe
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2023-11-08 23:57:49 -0500

    Chore(JJS/FRONT):Opcion de retiros, solo diseño

--- front/src/Dashboard/pages/Retiros/RetirosAdmin.tsx
+++ front/src/Dashboard/pages/Retiros/RetirosAdmin.tsx
@@ -1,12 +1,22 @@
 import './Retiros.css'
 import { NarbarAdmin } from '../../components/NarbarAdmin/NarbarAdmin'
+import { BarSession } from '../../components/barSession/barSession'
+import { ContainerRetiro } from '../../components/ContainerRetiro/ContainerRetiro'
 
 function RetirosAdmin() {
   return (
     <div className='retirosAdmin'>
-      
+
       <NarbarAdmin />
-      <p>Retiros</p>
+      <div className="conatinerRetiro">
+        <BarSession tituloVista='Blogs' segundoTitulo='Retiros' nombre='Kristine' img='../../../../src/assets/Dashboard-almacadaques/users/user.svg' />
+        <div className="infocontainerRetiro">
+          <ContainerRetiro/>
+          <ContainerRetiro/>
+        </div>
+        
+      </div>
+
     </div>
   )
 }
