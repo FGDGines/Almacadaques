41f710b91ab202460fef78c738cfc3feb8f17f1a
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2023-11-10 23:19:18 -0500

    Chore(JJS/FRONT):Funcionalidad de eliminar carrousel

--- front/src/Dashboard/components/ItemCarrusel/ItemCarrusel.tsx
+++ front/src/Dashboard/components/ItemCarrusel/ItemCarrusel.tsx
@@ -1,137 +1,99 @@
+import React, { useState } from 'react';
 import './ItemCarrusel.css';
+import itemsData from '../../../data/itemCarrousel';
 
+interface ContentItem {
+  src: string;
+  alt: string;
+}
+
+interface TextContent {
+  text: string;
+}
+
+interface Item {
+  index: number;
+  content: (ContentItem | {
+    content: {
+      content: TextContent[];
+    }[];
+    src?: never;
+    alt?: never;
+  })[];
+}
 
 export const ItemCarrusel = () => {
-    return (
-        <div className='ItemCarrusel'>
+  const [data, setData] = useState<Item[]>(itemsData);
 
-            <div className="barraCarrusel">
+  const handleDeleteItemCarrusel = (index: number) => {
+    const updatedData = data.filter((item) => item.index !== index);
+    setData(updatedData);
+  };
 
-                <div className="tituloCarrusel"><p>Item de Carrousel</p></div>
-                <div className="botonesCarrusel">
+  const handleUpdate = (index: number, newText: string) => {
+    const updatedData = data.map((item) => {
+      if (item.index === index && 'content' in item.content[0]) {
+        item.content[0].content[0].content[0].text = newText;
+      }
+      return item;
+    });
+    setData(updatedData);
+  };
 
-                    <i className='left'><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/left.svg" alt="Left" /></i>
-
-
-                    <i className='right'><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/right.svg" alt="Right" /></i>
-                </div>
-            </div>
-
-            <div className="itemsCarrusel">
-
-                <div className="items">
-                    <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg" alt="Fondo Items" />
-                    <div className="ctOver">
-
-                        <div className="iconosItems">
-                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Borrar.svg" alt="Borrar" /></i>
-                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Editar.svg" alt="Editar" /></i>
-                        </div>
-
-                        <div className="contenidoItems">
-                            <div className='ItemsDescripcion'>Es importante la salud mental</div>
-                            <div className="firma">
-                                firma
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div className="items">
-                    <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg" alt="Fondo Items" />
-                    <div className="ctOver">
-
-                        <div className="iconosItems">
-                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Borrar.svg" alt="Borrar" /></i>
-                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Editar.svg" alt="Editar" /></i>
-                        </div>
-
-                        <div className="contenidoItems">
-                            <div className='ItemsDescripcion'>Es importante la salud mental</div>
-                            <div className="firma">
-                                firma
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div className="items">
-                    <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg" alt="Fondo Items" />
-                    <div className="ctOver">
-
-                        <div className="iconosItems">
-                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Borrar.svg" alt="Borrar" /></i>
-                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Editar.svg" alt="Editar" /></i>
-                        </div>
-
-                        <div className="contenidoItems">
-                            <div className='ItemsDescripcion'>Es importante la salud mental</div>
-                            <div className="firma">
-                                firma
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div className="items">
-                    <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg" alt="Fondo Items" />
-                    <div className="ctOver">
-
-                        <div className="iconosItems">
-                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Borrar.svg" alt="Borrar" /></i>
-                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Editar.svg" alt="Editar" /></i>
-                        </div>
-
-                        <div className="contenidoItems">
-                            <div className='ItemsDescripcion'>Es importante la salud mental</div>
-                            <div className="firma">
-                                firma
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div className="items">
-                    <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg" alt="Fondo Items" />
-                    <div className="ctOver">
-
-                        <div className="iconosItems">
-                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Borrar.svg" alt="Borrar" /></i>
-                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Editar.svg" alt="Editar" /></i>
-                        </div>
-
-                        <div className="contenidoItems">
-                            <div className='ItemsDescripcion'>Es importante la salud mental</div>
-                            <div className="firma">
-                                firma
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div className="items">
-                    <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg" alt="Fondo Items" />
-                    <div className="ctOver">
-
-                        <div className="iconosItems">
-                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Borrar.svg" alt="Borrar" /></i>
-                            <i><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Editar.svg" alt="Editar" /></i>
-                        </div>
-
-                        <div className="contenidoItems">
-                            <div className='ItemsDescripcion'>Es importante la salud mental</div>
-                            <div className="firma">
-                                firma
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-
-
-            </div>
-
+  return (
+    <div className='ItemCarrusel'>
+      <div className="barraCarrusel">
+        <div className="tituloCarrusel"><p>Item de Carrousel</p></div>
+        <div className="botonesCarrusel">
+          <i className='left'><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/left.svg" alt="Left" /></i>
+          <i className='right'><img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/right.svg" alt="Right" /></i>
         </div>
-    );
+      </div>
+
+      <div className="itemsCarrusel">
+        {data.map((item, index) => (
+          <div key={index} className="items">
+            {item.content.map((contentItem, contentIndex) => (
+              <div key={contentIndex} className="ctOver">
+                {('src' in contentItem) && (
+                  <>
+                    <img src={contentItem.src} alt={contentItem.alt} />
+                    <div className="iconosItems">
+                      <i onClick={() => handleDeleteItemCarrusel(item.index)}>
+                        <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Borrar.svg" alt="Borrar" />
+                      </i>
+                      <i onClick={() => handleUpdate(item.index, 'Nuevo Texto')}>
+                        <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Editar.svg" alt="Editar" />
+                      </i>
+                    </div>
+                  </>
+                )}
+                {('content' in contentItem) && ('content' in contentItem.content[0]) && (
+                  <>
+                    <div className="iconosItems">
+                      <i onClick={() => handleDeleteItemCarrusel(item.index)}>
+                        <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Borrar.svg" alt="Borrar" />
+                      </i>
+                      <i onClick={() => handleUpdate(item.index, 'Nuevo Texto')}>
+                        <img src="../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/Editar.svg" alt="Editar" />
+                      </i>
+                    </div>
+                    <div className="contenidoItems">
+                      {contentItem.content[0].content.map((textContent: TextContent, textIndex: number) => (
+                        <React.Fragment key={textIndex}>
+                          <div className='ItemsDescripcion'>
+                            {textContent.text}
+                          </div>
+                        </React.Fragment>
+                      ))}
+                    </div>
+                  </>
+                )}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
--- front/src/data/itemCarrousel.ts
+++ front/src/data/itemCarrousel.ts
@@ -1,25 +1,57 @@
 // itemCarrousel.ts
+
 const itemsData = [
-    {
-        "index": 0,
-        "content": [
+  {
+    index: 0,
+    content: [
+      {
+        src: "../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg",
+        alt: "Fondo Items",
+      },
+      {
+        content: [
           {
-            "src": "../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg",
-            "alt": "Fondo Items"
+            content: [
+              { text: "Es importante la salud mental" },
+              { text: "firma" },
+            ],
           },
+        ],
+      },
+    ],
+  },
+  {
+    index: 1,
+    content: [
+      {
+        src: "../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg",
+        alt: "Fondo Items",
+      },
+      {
+        content: [
           {
-            "content": [
-              {
-                "content": [
-                  { "text": "Es importante la salud mental" },
-                  { "text": "firma" }
-                ]
-              }
-            ]
-          }
-        ]
-    }
-  ];
-  
-  export default itemsData;
-  
+            content: [{ text: "Ante todo la salud" }, { text: "Autor" }],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    index: 2,
+    content: [
+      {
+        src: "../../../../src/assets/Dashboard-almacadaques/ItemCarrusel/fondo-items.svg",
+        alt: "Fondo Items",
+      },
+      {
+        content: [
+          {
+            content: [{ text: "Ante todo la salud" }, { text: "Autor" }],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default itemsData;
