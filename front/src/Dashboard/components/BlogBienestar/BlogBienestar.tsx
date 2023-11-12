45c4de27860bc2246501a4bf62f94e70cfe5991d
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2023-11-12 14:36:32 -0500

    Chore(JJS/FRONT):Subiendo blog de Bienestar admin

--- front/src/Dashboard/components/BlogBienestar/BlogBienestar.tsx
+++ front/src/Dashboard/components/BlogBienestar/BlogBienestar.tsx
@@ -1,15 +1,39 @@
-import { NarbarAdmin } from "../NarbarAdmin/NarbarAdmin"
-import "./BlogBienestar.css"
+import { useState } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { NarbarAdmin } from "../NarbarAdmin/NarbarAdmin";
+import { BarSession } from "../barSession/barSession";
+import "./BlogBienestar.css";
 
-function BlogBienestar() {
+const BlogBienestar = () => {
+  const [editorData, setEditorData] = useState('');
+
+  const handleEditorChange = (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    _event: any,
+    editor: { getData: () => string }
+  ) => {
+    const data = editor.getData();
+    setEditorData(data);
+  };
+
   return (
     <div className="BlogBienestar">
-        <NarbarAdmin />
-        <div className="containerBlogBienestar">
-            
+      <NarbarAdmin />
+      <div className="containerBlogBienestar">
+        <BarSession tituloVista='Blog' segundoTitulo='Tips Bienestar' nombre='Kristine' img='../../../../src/assets/Dashboard-almacadaques/users/user.svg' />
+        <div className="editordeLibro">
+          {/* Agrega el componente CKEditor aquí */}
+          <CKEditor
+            editor={ClassicEditor}
+            data={editorData}
+            onChange={(event, editor) => handleEditorChange(event, editor)}
+          />
         </div>
-    </div>
-  )
-}
 
-export  {BlogBienestar}
+      </div>
+    </div>
+  );
+};
+
+export { BlogBienestar };
