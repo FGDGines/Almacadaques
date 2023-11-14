37bdb2d3d15119c13414d0e9261950360b2f4e4b
FranGines <Corunddev@gmail.com>
2023-11-14 17:22:58 +0100

    chore(Fran/front): comento codigo por si lo necesitamos no borrarlo

--- front/src/components/Colaboradores/Colaboradores.tsx
+++ front/src/components/Colaboradores/Colaboradores.tsx
@@ -2,14 +2,14 @@ import './Colaboradores.css'
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import Franja from '../Franja/Franja';
-import { textos } from '../../data/textos';
-import { GlobalContext } from '../../contexts/GlobalContext';
-import { useContext } from 'react';
+// import { textos } from '../../data/textos';
+// import { GlobalContext } from '../../contexts/GlobalContext';
+// import { useContext } from 'react';
 import { Tarjetacolaboradores } from '../TarjetaColaboradores/Tarjetacolaboradores';
 import { ListaColaboradores } from '../../data/colaboradores';
 
 function Colaboradores() {
-    const { languageFlag } = useContext(GlobalContext)
+    // const { languageFlag } = useContext(GlobalContext)
     return (
         <>
             <Navbar />
