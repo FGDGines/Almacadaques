a6f65ebeb5889501c83146409df3e8d960edf4ba
David Lezcano Becerra <davidbeceralezcano@gmail.com>
2023-11-26 23:53:04 -0500

    chore(DLB/front): conectado calendar event en clientes

--- front/src/components/Agenda/Agenda.tsx
+++ front/src/components/Agenda/Agenda.tsx
@@ -2,9 +2,10 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
 import './Agenda.css';
-import { tpAgenda, tpCalendarDates } from '../../types/typesComponents';
+import { tpAgenda, tpCalendarDates, tpCalendarEvent, tpDtmResponse } from '../../types/typesComponents';
 import { eventos } from '../../data/calendar';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { fetchDefault } from '../../helpers/Server';
 
 // Configuración de localización utilizando moment.js
 moment.locale('es');
@@ -12,7 +13,7 @@ const localizer = momentLocalizer(moment);
 
 const Agenda: FC<tpAgenda> = ({hSelect}) => {
   // Eventos de ejemplo (puedes reemplazarlos con tus propios datos)
-
+  const [calendarEvent, setCalendarEvent] = useState<tpCalendarEvent[]>([]);
   
   const eventsWithDates: tpCalendarDates[] = eventos.map(event => ({
     id: event.id, 
@@ -27,11 +28,36 @@ const Agenda: FC<tpAgenda> = ({hSelect}) => {
       hSelect(event)
   };
 
+
+  useEffect(() => {
+    const api = async () => {
+      const calendars: tpCalendarEvent[] = []
+      fetchDefault("/calendar_event/read", {}, (d: tpDtmResponse) => {
+        if(!d.bag) return 
+        for (let index = 0; index < d.bag.length; index++) {
+          const element: {id: number , titulo: string, descripcion: string, inicio: number, final: string } = d.bag[index];
+            const value = { 
+              id: element.id, 
+              title: element.titulo,
+              description: element.descripcion , 
+              start: new Date(element.inicio),
+              end: new Date(element.final),
+            }
+            calendars.push(value)
+        }
+        console.log(calendars)
+        setCalendarEvent(calendars);
+      }) 
+    };
+    api();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div style={{ height: '100%' }}>
       <Calendar
         localizer={localizer}
-        events={eventsWithDates}
+        events={calendarEvent}
         startAccessor="start"
         endAccessor="end"
         views={['month']}
