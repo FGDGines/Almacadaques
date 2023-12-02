ec7ef184bed9fd83daf9440431f3c4de20bbeb25
David Lezcano Becerra <davidbeceralezcano@gmail.com>
2023-12-02 12:34:21 -0500

    fix(DLB/front): size de las imagenes en los formularios

--- front/src/Dashboard/components/Formularios/FormularioBlogBienestar/FormularioBlogBienestar.tsx
+++ front/src/Dashboard/components/Formularios/FormularioBlogBienestar/FormularioBlogBienestar.tsx
@@ -131,7 +131,7 @@ function FormularioBlogBienestar() {
                     <form className='formCarrousel'>
                         <div className="subirArchivos">
                             <label htmlFor="File" className='labelArchivo'>
-                                <img src={imageURL || ""} alt="Selected" />
+                                <img src={imageURL || ""}  className="img" alt="Selected" />
                                 <span className='arrastra'>Arrastra y suelta o <span>sube</span> </span>
                                 <span className='formatos'>Supported formates: JPEG, PNG, GIF, MP4, PDF, PSD, AI, Word, PPT</span>
                             </label>
--- front/src/Dashboard/components/Formularios/FormularioCarrousel/FormularioCarrousel.tsx
+++ front/src/Dashboard/components/Formularios/FormularioCarrousel/FormularioCarrousel.tsx
@@ -90,6 +90,7 @@ export const FormularioCarrousel = () => {
             Url: '',
             archivo: null,
         })
+        setImageURL("")
     };
 
 
@@ -132,7 +133,7 @@ export const FormularioCarrousel = () => {
                 <form className='formCarrousel'>
                     <div className="subirArchivos">
                         <label htmlFor="File" className='labelArchivo'>
-                            <img src={imageURL || ""} alt="Selected" />
+                            <img src={imageURL || ""} className="img" alt="Selected" />
                             <span className='arrastra'>Arrastra y suelta o <span>sube</span> </span>
                             <span className='formatos'>Supported formates: JPEG, PNG, GIF, MP4, PDF, PSD, AI, Word, PPT</span>
                         </label>
