47faf9bb8300bdcc422fa3feea6d9972924c54b2
David Lezcano Becerra <davidbeceralezcano@gmail.com>
2023-12-05 00:36:56 -0500

    chore(DLB/front): cambios a libros v2

--- front/src/components/BlogPost/BlogPost.tsx
+++ front/src/components/BlogPost/BlogPost.tsx
@@ -11,6 +11,7 @@ import { fetchDefault } from '../../helpers/Server';
 
 import portImg from "../../../src/assets/ImgLibro/portada.jpeg"
 
+
 const BlogPost = () => {
   const [data, setData] = useState<tpTextLibro[]>([]);
   const [current, setCurrent] = useState<tpTextLibro | null>(null);
@@ -44,6 +45,8 @@ const BlogPost = () => {
     api();
     // eslint-disable-next-line
   }, []);
+
+
 
 
   return (
@@ -82,66 +85,186 @@ const BlogPost = () => {
 
         >
 
-          <div className="demoPage" >
-              <div className="port">
-                  <div className="titlePortal2">
-                      <img src={portImg} alt="" />
-                      <div className="ctOverlayBook">
-                          <div className="titleCortoPor">
-                              <h1>{current?.title}</h1>
-                              <h3>{current?.subtitle}</h3>
-                          </div>
-                          <p>Elisabet Coll-Vinent</p>
-                      </div>
-                  </div>
-              </div>
+
+
+    <div className="demoPage" >
+        <div className="port">
+            <div className="titlePortal2">
+                <img src={portImg} alt="" />
+                <div className="ctOverlayBook">
+                    <div className="titleCortoPor">
+                        <h1>{current?.title}</h1>
+                        <h3>{current?.subtitle}</h3>
+                    </div>
+                    <p>Elisabet Coll-Vinent</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className="demoPage" >
+      <div className="port">
+
+        <div className="titlePortal2">
+          <div className="ctOverlayBook">
+          <img src={portImg} alt="" />
           </div>
-          <div className="demoPage" >
-              <div className="port">
+        </div>
+      </div>
+    </div>
 
-                  <div className="titlePortal2">
-                      <img src={portImg} alt="" />
-                      <div className="ctOverlayBook">
-
-
-                      </div>
-                  </div>
+    {current?.content ? current?.content.map((content, index) => (
+     
+        <div className="demoPage" >
+          <div className="marco">
+            <div className="titlePortal">
+              {index == 0 ? (
+                <div className="imgLibro">
+                  <img src="../../../src/assets/ImgLibro/1.jpg" alt="" />
+                </div>
+              ) : <></>}
+              <div className="titlePortal">
+                {content}
               </div>
+
+
+            </div>
           </div>
 
-          {current?.content ? current?.content.map((content, index) => (
-            <div className="demoPage" >
-              <div className="port">
-                {index == 0 ? (
-                  <div className="imgLibro">
-                  <img src="../../../../src/assets/ImgLibro/3.png" alt="" />
-              </ div>
-                ) : <></>}
-                <div className="titlePortal2">
-                  <div dangerouslySetInnerHTML={{ __html: content }} />
-                  
+        </div>
+          
+    )) : <></>}
+<div className="demoPage">
+            <div className="port">
+
+              <div className="titlePortal2">
+              <img src="../../../src/assets/ImgLibro/portada.jpeg" alt="" />
+                <div className="ctOverlayBook">
+
                 </div>
               </div>
             </div>
-          )) : <></>}
- 
+          </div>
+          <div className="demoPage">
+            <div className="port">
+
+              <div className="titlePortal2">
+              <img src="../../../src/assets/ImgLibro/portada.jpeg" alt="" />
+                <div className="ctOverlayBook">
+                  <h3 className='FinalLibro'>© Elisabet Coll-Vinent@almacadaques.com</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* 
+          <div className="demoPage" >
+            <div className="marco">
+              <div className="titlePortal">
+                <div className="imgLibro">
+                  <img src="../../../src/assets/ImgLibro/1.jpg" alt="" />
+                </div>
+                <div className="titlePortal">
+                  <h4>TIPS BIENESTAR. ARTICULO 1.</h4>
+                  <h3>PARAR </h3>
+                  <p>En las experiencias de bienestar que organizamos desde @almacadaques uno de los elementos. </p>
+
+                </div>
+
+
+              </div>
+            </div>
+
+          </div>
+          <div className="demoPage" >
+
+            <div className="marco">
+              <div className="titlePortal">
+<p> más importantes que promovemos es PARAR.</p>
+                <p>5 tips para parar y dedicarte tiempo para ti:</p>
+                <h3>1.Establece límites:  </h3>
+                <p>Parar y establecer límites te permite mantener un equilibrio entre tus responsabilidades y tu bienestar.
+                  Al establecer límites, evitas el agotamiento y el estrés excesivo, lo que te permite tener más energía y disfrutar de la vida.</p>
+                <h3>2. Prioriza tu bienestar: </h3>
+                <p>Dedicarte tiempo para ti te ayuda a cuidar de tu salud física y mental.
+                  Al hacerlo, fortaleces tu sistema inmunológico, reduces el riesgo de enfermedades relacionadas con el estrés y mejora tu estado de ánimo.</p>
+
+              </div>
+            </div>
+
+          </div>
+          <div className="demoPage" >
+
+            <div className="marco">
+              <div className="titlePortal">
+
+                <h3>3. Fomenta la creatividad </h3>
+                <p>Al parar y dedicarte tiempo para ti, estimulas tu creatividad y exploras nuevas ideas. La creatividad es fundamental para resolver problemas, encontrar nuevas soluciones y mejorar tu calidad de vida.</p>
+                <h3>4. Mejora tus relaciones</h3>
+                <p>Al cuidarte a ti mism@, tienes más energía y disposición para relacionarte con los demás de manera saludable.
+                  Tener tiempo para ti te permite recargar energías y estar presente en tus relaciones, lo que fortalece los vínculos afectivos.</p>
+
+              </div>
+            </div>
+
+          </div>
+
+          <div className="demoPage" >
+
+            <div className="marco">
+              <div className="titlePortal">
+
+                <h3>5. Autoconocimiento y crecimiento personal </h3>
+                <p>Parar y dedicarte tiempo para ti te ayuda a conocerte mejor, identificar tus necesidades y objetivos personales.
+                  El autoconocimiento es la base para el crecimiento personal y el logro de metas, lo que te lleva a una vida más plena y satisfactoria.</p>
+
+                <p>¿Y tu, te permites parar? </p>
+                <h3>@almacadaques, setiembre 2023</h3>
+
+
+              </div>
+            </div>
+
+          </div>
+
+
+
+
+
+
+          <div className="demoPage">
+            <div className="port">
+
+              <div className="titlePortal2">
+              <img src="../../../src/assets/ImgLibro/portada.jpeg" alt="" />
+                <div className="ctOverlayBook">
+
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="demoPage">
+            <div className="port">
+
+              <div className="titlePortal2">
+              <img src="../../../src/assets/ImgLibro/portada.jpeg" alt="" />
+                <div className="ctOverlayBook">
+                  <h3 className='FinalLibro'>© Elisabet Coll-Vinent@almacadaques.com</h3>
+                </div>
+              </div>
+            </div>
+          </div> */}
+
+
 
         </HTMLFlipBook>
-
         <div className="ItemBookUser">
           <div className="carduser">
             <ul className='UlLista'>
-
-            {data.map((text) => (
-              <a href="#" className='EnlacesItem ' onClick={() => setCurrent(text)}>{text.title}</a>
-            ))}
-              
-
-              {/* <a href="#" className='EnlacesItem ' onClick={() => setLayoutID(5)}>5 Tips Parar</a>
+              <a href="#" className='EnlacesItem ' onClick={() => setLayoutID(5)}>5 Tips Parar</a>
               <a href="#" className='EnlacesItem' onClick={() => setLayoutID(33)}>5 Tips Abundancia</a>
-              <a href="#" className='EnlacesItem' onClick={() => setLayoutID(34)}>5 Tips Emociones</a>
-              <a href="#" className='EnlacesItem' onClick={() => setLayoutID(35)}>5 Tips lidiar dia a dia </a>
-              <a href="#" className='EnlacesItem' onClick={() => setLayoutID(36)}>5 Tips para Fluir</a> */}
+              <a href="#" className='EnlacesItem' onClick={() => setLayoutID(34)}>5 tips Emociones</a>
+              <a href="#" className='EnlacesItem' onClick={() => setLayoutID(35)}>5 tips lidiar dia a dia </a>
+              <a href="#" className='EnlacesItem' onClick={() => setLayoutID(36)}>5 tips para fluir</a>
             </ul>
           </div>
         </div>
